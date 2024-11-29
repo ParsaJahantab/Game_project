76ba1f55092aecb19e87534a486768a4d80d426e
Parsa <parsaj1381@gmail.com>
2024-11-29 15:35:07 +0330

    all_puzzles

--- puzzles_functions.py
+++ puzzles_functions.py
@@ -3,23 +3,23 @@ from battleship import *
 from core.app import App
 
 
-# def door():
-#     app = App("door.home", door="door0")
-#     app.set_state("solved", False)
-#     app.run()
-#     return app.get_state("solved")
+def door():
+    app = App("door.home", door="door0")
+    app.set_state("solved", False)
+    app.run()
+    return app.get_state("solved")
 
 
-# def hats():
-#     app = App("hat.home")
-#     app.set_state("solved", False)
-#     app.run()
-#     return app.get_state("solved")
+def hats():
+    app = App("hat.home")
+    app.set_state("solved", False)
+    app.run()
+    return app.get_state("solved")
 
 
-# def battleship(g):
-#     game = BattleshipGame(g)
-#     return game.run()
+def battleship(g):
+    game = BattleshipGame(g)
+    return game.run()
 
 
 def dragons():
@@ -29,8 +29,8 @@ def dragons():
     return app.get_state("solved")
 
 
-# def matchstick():
-#     app = App("matchstick.home")
-#     app.set_state("solved", False)
-#     app.run()
-#     return app.get_state("solved")
+def matchstick():
+    app = App("matchstick.home")
+    app.set_state("solved", False)
+    app.run()
+    return app.get_state("solved")
