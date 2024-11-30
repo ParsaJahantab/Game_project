694ecb0f3aa7877cac4234fee7f25776eee1c38e
Parsa <parsaj1381@gmail.com>
2024-11-30 11:50:15 +0330

    added all puzzles

--- puzzles_functions.py
+++ puzzles_functions.py
@@ -3,42 +3,42 @@ from battleship import *
 from core.app import App
 
 
-# def door():
-#     app = App("door.home", door="door0")
-#     app.set_state("solved", False)
-#     app.run()
-#     return app.get_state("solved")
+def door():
+    app = App("door.home", door="door0")
+    app.set_state("solved", False)
+    app.run()
+    return app.get_state("solved")
 
 
-# def hats():
-#     app = App("hat.home")
-#     app.set_state("solved", False)
-#     app.run()
-#     return app.get_state("solved")
+def hats():
+    app = App("hat.home")
+    app.set_state("solved", False)
+    app.run()
+    return app.get_state("solved")
 
 
-# def battleship(g):
-#     game = BattleshipGame(g)
-#     return game.run()
+def battleship(g):
+    game = BattleshipGame(g)
+    return game.run()
 
 
-# def dragons():
-#     app = App("dragon.home")
-#     app.set_state("solved", False)
-#     app.run()
-#     return app.get_state("solved")
+def dragons():
+    app = App("dragon.home")
+    app.set_state("solved", False)
+    app.run()
+    return app.get_state("solved")
 
-# def cannibal():
-#     app = App("cannibal.home")
-#     app.set_state("solved", False)
-#     app.run()
-#     return app.get_state("solved")
+def cannibal():
+    app = App("cannibal.home")
+    app.set_state("solved", False)
+    app.run()
+    return app.get_state("solved")
 
-# def matchstick():
-#     app = App("matchstick.home")
-#     app.set_state("solved", False)
-#     app.run()
-#     return app.get_state("solved")
+def matchstick():
+    app = App("matchstick.home")
+    app.set_state("solved", False)
+    app.run()
+    return app.get_state("solved")
 
 def chest(key,player):
     if not key:
